27c782b8b5e0daea4e78bd90e9982c5e73db4807
yamseey <malaakrafia@gmail.com>
2026-03-25 18:58:58 +0200

    Show item list/info

--- app.py
+++ app.py
@@ -11,13 +11,19 @@ app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    all_items=items.get_items()
+    return render_template("index.html", items = all_items)
+
+@app.route("/item/<int:item_id>")
+def show_item(item_id):
+    item=items.get_item(item_id)
+    return render_template("show_item.html", item=item)
 
 @app.route("/new_item")
 def new_item():
     return render_template("new_item.html")
 
-@app.route("/create", methods=["POST"])
+@app.route("/create_item", methods=["POST"])
 def create_item():
     title = request.form["title"]
     description = request.form["description"]
--- items.py
+++ items.py
@@ -3,3 +3,17 @@ import db
 def add_item(title, description, author, user_id):
     sql = """INSERT INTO items (title, description, author, user_id) VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, description, author, user_id])
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.title,
+                    items.description,
+                    items.author,
+                    users.username
+                FROM items, users
+                WHERE items.user_id = users.id AND
+                    items.id = ?"""
+    return db.query(sql, [item_id])[0]
