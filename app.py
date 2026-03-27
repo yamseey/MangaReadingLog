828c8ab44ba064664d38c8cbadab5b96d526ce8e
yamseey <malaakrafia@gmail.com>
2026-03-27 14:03:21 +0200

    Improve user access checks

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ import items
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
+def require_login():
+    if "user_id" not in session:
+        abort(403)
+
 @app.route("/")
 def index():
     all_items=items.get_items()
@@ -34,10 +38,13 @@ def show_item(item_id):
 
 @app.route("/new_item")
 def new_item():
+    require_login()
     return render_template("new_item.html")
 
 @app.route("/create_item", methods=["POST"])
 def create_item():
+    require_login()
+
     title = request.form["title"]
     description = request.form["description"]
     author = request.form["author"]
@@ -48,6 +55,7 @@ def create_item():
 
 @app.route("/edit_item/<int:item_id>")
 def edit_item(item_id):
+    require_login()
     item=items.get_item(item_id)
     if not item:
         abort(404)
@@ -57,6 +65,7 @@ def edit_item(item_id):
 
 @app.route("/update_item", methods=["POST"])
 def update_item():
+    require_login()
     item_id=request.form["item_id"]
     item=items.get_item(item_id)
     if not item:
@@ -74,6 +83,7 @@ def update_item():
 
 @app.route("/remove_item/<int:item_id>", methods = ["GET", "POST"])
 def remove_item(item_id):
+    require_login()
     item = items.get_item(item_id)
     if not item:
         abort(404)
