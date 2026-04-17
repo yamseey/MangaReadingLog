5cd12a0e5f9b0a2f0374d93ceec5f1066869238e
yamseey <malaakrafia@gmail.com>
2026-04-17 23:59:17 +0300

    Fix item removal

--- items.py
+++ items.py
@@ -61,6 +61,8 @@ def update_item(item_id, title, description, author, classes):
         db.execute(sql, [item_id, title, value])
 
 def remove_item(item_id):
+    sql = "DELETE FROM item_classes WHERE item_id = ?"
+    db.execute (sql, [item_id])
     sql = "DELETE FROM items WHERE id = ?"
     db.execute (sql, [item_id])
 
